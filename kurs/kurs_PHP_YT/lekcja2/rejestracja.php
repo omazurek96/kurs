af264c852a20d72d42d5c62a721679f8f70249a3
omazurek96 <omazurek96@gmail.com>
2018-03-24 10:08:22 +0100

    Nowe zadanie

---
+++ kurs/kurs_PHP_YT/lekcja2/rejestracja.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Osadnicy - gra przeglądarkowa</title>
+</head>
+<body>
+
+<?php
+
+?>
+</body>
+</html>
